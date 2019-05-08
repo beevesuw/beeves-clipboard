89393f04836a2387a21d0096f3b77b2a67663f24
Aditya Chatterjee <adityachatterjee42@gmail.com>
2019-05-07 17:02:06 -0700

    made beevesfile

---
+++ content-script.js
@@ -0,0 +1,12 @@
+function copySelection() {
+    var selectedText = window.getSelection().toString().trim();
+
+    if (selectedText) {
+        document.execCommand("Copy");
+    }
+}
+
+/*
+Add copySelection() as a listener to mouseup events.
+*/
+document.addEventListener("mouseup", copySelection);
