0b15bc1daac68d4d7b70a4af3cae9e8f506c0e40
Aditya Chatterjee <adityachatterjee42@gmail.com>
2019-05-11 16:16:52 -0700

    paste as alert

--- content-script.js
+++ content-script.js
@@ -1,16 +1,26 @@
-async function copySelection() {
-    var selectedText = window.getSelection().toString().trim();
+function copy(keyword) {
+    let selectedText = window.getSelection().toString().trim();
     if (selectedText) {
-        alert(selectedText);
+        clipboard[keyword] = selectedText;
     }
 }
 
+function paste(keyword) {
+    alert(clipboard[keyword]);
+}
+
+
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
     if(message.action == 'copy'){
-        alert(`copy: ${message.keyword}`);
-        copySelection();
+        //alert(`copy: ${message.keyword}`);
+        copy(message.keyword.toLowerCase());
     }
     else if(message.action == 'paste'){
-        alert(`paste: ${message.keyword}`);
+        //alert(`paste: ${message.keyword}`);
+        paste(message.keyword.toLowerCase());
+
+
     }
 });
+
+clipboard = {};
